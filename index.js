2664e2d08d17626d0f5e09eb1da8d076de64b1b8
penina wanjiru <penina.wanjiru@student.moringaschool.com>
2022-06-06 02:20:31 +0300

    commit tree dc18957edabe1a843a9ad8e0a63c8c6350698bfc

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+let employee={name:"penina",streetAddress:"Nairobi"};
+
+function deleteFromEmployeeByKey(employee, key){
+    var newEmployee = Object.assign({}, employee);
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+  return employee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+  return employee;
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    var newEmployee = Object.assign({}, employee, {[key]: value});
+    return newEmployee;
+}
+
